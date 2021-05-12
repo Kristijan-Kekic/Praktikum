a62946860204208f83a4b3d9a3dbe9cca27678d7
Kristijan-Kekic <73902816+Kristijan-Kekic@users.noreply.github.com>
2021-05-12 13:21:49 +0200

    Add files via upload

--- src/priceListFormatter.js
+++ src/priceListFormatter.js
@@ -1,27 +1,30 @@
-/*let data = [
-    {from: "2020-01-01", to: "2020-02-01", price: 34.5},
-    {from: "2020-02-02", to: "2020-03-01", price: 37.0},
-    {from: "2020-03-02", to: "2020-05-15", price: 39.0},
-    {from: "2020-05-16", to: "2020-06-15", price: 37.0},
-   ];
-*/
-
-function priceListFormatter(data) {
-    const data2 =  [...data].sort((a, b) => a.price - b.price );
-    const output = createOutput(data2);
+function priceListFormatter(pricelist) {
+  
+    const orderedDataByPrice = [...pricelist].sort((a, b) => a.price - b.price);
+    const dataCloneMap = createMapByPrice(orderedDataByPrice);
+    const output = createOutput(dataCloneMap);
+  
     console.log(output);
-}
+  }
+  
+  function createMapByPrice(pricelist) {
+    new Map();
+    return pricelist.reduce((map, element) => {
+      const Dates = ({ from, to } = element);
+      const Cost = map.get(element.price);
+      map.set(element.price, Cost ? [...Cost, Dates] : [Dates]);
+      return map;
+    });
+  }
+  
+  function createOutput(pricelist) {
+    return Array.from(pricelist)
+      .reduce((output, [cost, value]) => {
+        const Dates = value.map((Dates) => `${Dates.from} do ${Dates.to}`).join(' , ');
+        return (output += `${cost} : ${Dates}\n`);
+      }, '')
+  }
 
-function createOutput(data) {
-    let output = '';
-    data.forEach((item, i, arr) => {
-    const dateStr = `${item.from} do ${item.to}`;
-    output +=
-    arr[i - 1]?.price === item.price
-    ? ` , ${dateStr}`
-    : `\n${item.price.toFixed('1')} : ${dateStr}`;
-})}
-
-return output;
-   
-module.exports = priceListFormatter;
+  
+  
+  module.exports = priceListFormatter;
