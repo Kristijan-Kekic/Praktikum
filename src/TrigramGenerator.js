79f09930fd54dc3aada80fe4042038e2d586d2a3
Kristijan-Kekic <73902816+Kristijan-Kekic@users.noreply.github.com>
2021-05-26 12:52:26 +0200

    Add files via upload

---
+++ src/TrigramGenerator.js
@@ -0,0 +1,48 @@
+function trigramGenerator(text) {
+  if (typeof text !== 'string') throw new Error();
+
+  const words = interpunctionHandler(text).split(' ');
+  
+  let chunks = generateChunks(lowerCaseSveOsimI(words));
+
+  return generateTrigrams(chunks);
+}
+
+function lowerCaseSveOsimI(wordArray) {
+  return wordArray.map( (word) => word != 'I' ? word.toLowerCase() : word );
+}
+
+function whitespaceHandler(text) {
+  return text.replace(/\s+/g, ' ').trim(); 
+}
+
+function interpunctionHandler(text){
+  text = text.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, ' ');
+  return whitespaceHandler(text);
+}
+
+function generateChunks(words) {
+  let array = words.slice(0, words.length - 2);
+  let result = [];
+
+  array.forEach((_word,i) => {
+      result.push(words.slice(i, i + 3));
+  });
+
+  return result;
+}
+
+function generateTrigrams(words) {
+  return words.reduce((acc, value) => {
+      let key = value[0] + ' ' + value[1];
+      if(!acc.has(key)) acc.set(key, [])
+      acc.get(key).push(value[2]);
+
+      return acc;
+  }, new Map());
+}
+
+
+
+
+module.exports = {trigramGenerator, generateChunks, whitespaceHandler, interpunctionHandler};
